55d0bf5cb58c7e041b12d3e326ceebf2ecd07e5f
leesena <tpsk1545@gmail.com>
2025-11-25 17:49:55 +0900

    1124 과제

---
+++ js/slder.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const slideWrapper = document.querySelector('.testimonial');
+    const slideContainer = slideWrapper.querySelector('ul');
+    const slides = slideContainer.querySelectorAll('li');
+ });
